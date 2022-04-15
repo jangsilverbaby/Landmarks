fcb662c5a2bdc51698a0e12432f6f203ccb7439a
jangsilverbaby <dmsdo1234@gmail.com>
2022-04-15 15:18:29 +0900

    Section4 Create a Custom Image View

---
+++ Landmarks/CircleImage.swift
@@ -0,0 +1,25 @@
+//
+//  CircleImage.swift
+//  Landmarks
+//
+//  Created by 장은애(Eunae Jang) on 2022/04/15.
+//
+
+import SwiftUI
+
+struct CircleImage: View {
+    var body: some View {
+        Image("turtlerock")
+            .clipShape(Circle()) // 이미지를 원형으로 깎기
+            .overlay {
+                Circle().stroke(.white, lineWidth: 4)
+            } // 원을 만든 후 이미지를 오버레이로 추가하여 테두리 지정
+            .shadow(radius: 7) // 그림자
+    }
+}
+
+struct CircleImage_Previews: PreviewProvider {
+    static var previews: some View {
+        CircleImage()
+    }
+}

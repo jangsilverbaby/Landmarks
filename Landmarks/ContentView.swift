9f98d29ee3b3b9e3f0cc6ee71f7529cffd9af412
jangsilverbaby <dmsdo1234@gmail.com>
2022-04-15 12:38:56 +0900

    Section2 Customize the Text View

--- Landmarks/ContentView.swift
+++ Landmarks/ContentView.swift
@@ -9,8 +9,9 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello SwiftUI!")
-            .padding()
+        Text("Tutle Rock")
+            .font(.title) // Command + click : User the insperctor
+            .foregroundColor(.black)
     }
 }
 

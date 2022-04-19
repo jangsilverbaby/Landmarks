3c3aee4f04fd4792a973b5b6e1a591558f849b01
jangsilverbaby <dmsdo1234@gmail.com>
2022-04-19 16:30:41 +0900

    Section6 Create a Favorite Button for Each Landmark

--- Landmarks/Views/Landmarks/LandmarkList.swift
+++ Landmarks/Views/Landmarks/LandmarkList.swift
@@ -10,19 +10,20 @@ import SwiftUI
 struct LandmarkList: View {
     @EnvironmentObject var modelData: ModelData
     @State private var showFavoritesOnly = false
-    
+
     var filteredLandmarks: [Landmark] {
         modelData.landmarks.filter { landmark in
             (!showFavoritesOnly || landmark.isFavorite)
         }
     }
-    
+
     var body: some View {
         NavigationView {
-            List{
+            List {
                 Toggle(isOn: $showFavoritesOnly) {
                     Text("Favorites only")
                 }
+
                 ForEach(filteredLandmarks) { landmark in
                     NavigationLink {
                         LandmarkDetail(landmark: landmark)
@@ -39,5 +40,6 @@ struct LandmarkList: View {
 struct LandmarkList_Previews: PreviewProvider {
     static var previews: some View {
         LandmarkList()
+            .environmentObject(ModelData())
     }
 }

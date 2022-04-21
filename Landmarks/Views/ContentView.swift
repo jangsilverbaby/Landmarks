7529f2e9c4c490b03db3dca917063a74f1eee4f0
jangsilverbaby <dmsdo1234@gmail.com>
2022-04-21 09:17:20 +0900

    Change ContentView.swift

--- Landmarks/Views/ContentView.swift
+++ Landmarks/Views/ContentView.swift
@@ -9,41 +9,13 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            MapView()
-                .ignoresSafeArea(edges: .top)
-                .frame(height: 300)
-            
-            CircleImage()
-                .offset(y: -130)
-                .padding(.bottom, -130)
-            VStack(alignment: .leading) { // Stack : 여러 뷰를 겷합하고 포함할 수 있음.
-                Text("Tutle Rock")
-                    .font(.title) // Command + click : User the insperctor
-                HStack {
-                    Text("Joshua Tree National Park")
-                        .font(.subheadline)
-                    Spacer() // 크기를 정의하는 대신 상위 뷰의 모든 공간을 사용하도록 확장
-                    Text("California")
-                        .font(.subheadline)
-                }
-                .font(.subheadline)
-                .foregroundColor(.secondary)
-                
-                Divider()
-                
-                Text("About Turtle Rock")
-                    .font(.title2)
-                Text("Descriptive text goes here.")
-            }
-            .padding()
-            Spacer()
-        }
+        LandmarkList()
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
+            .environmentObject(ModelData())
     }
 }
